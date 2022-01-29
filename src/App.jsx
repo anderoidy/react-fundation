21f0517e58334bf741ec26346082404355767d88
anderoidy <anderoidy@gmail.com>
2022-01-29 16:42:00 -0300

    finalizado primeira parte

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,57 @@
-import './App.css';
+import "./App.css";
 import React from "react";
 
-import Primeiro from './components/Primeiro'
-import CompParametro from './components/CompParametro'
-import CompFilhos from './components/CompFilhos'
-import Card from './components/layout/Card';
+import Primeiro from "./components/Primeiro";
+import CompParametro from "./components/CompParametro";
+import CompFilhos from "./components/CompFilhos";
+import Card from "./components/layout/Card";
+import Repeticao from "./components/Repeticao";
+import CondicionalComTernario from "./components/CondicionalComTernario";
+import CondicionalComIf from "./components/CondicionalComIf";
 
 export default (props) => (
-    <div className="App">
-    <Card titulo="Componente com Filhos">
-        <CompFilhos>
+  <div className="App">
+
+    <Card titulo="#06 - Condicional v2">
+      <CondicionalComIf numero={15}></CondicionalComIf>
+    </Card>
+
+    <Card titulo="#05 - Condicional v1">
+      <CondicionalComTernario numero={10}></CondicionalComTernario>
+    </Card>
+
+    <Card titulo="#04 - Repetição">
+      <Repeticao></Repeticao>
+    </Card>
+
+    <Card titulo="#03 - Componente com Filhos">
+      <CompFilhos>
         <ul>
-                <li>Ana</li>
-                <li>Bia</li>
-                <li>Carlos</li>
-                <li>Daniel</li>
-                <li>Joao</li>
-                <li>Sandro</li>
-            </ul>
-        </CompFilhos>       
+          <li>Ana</li>
+          <li>Bia</li>
+          <li>Carlos</li>
+          <li>Daniel</li>
+          <li>Joao</li>
+          <li>Sandro</li>
+        </ul>
+      </CompFilhos>
     </Card>
-    <Card titulo="Compontente com Parametro">
-        <CompParametro titulo="Esse é o Titulo" subtitulo="Esse é o Subtitulo" />
+
+    <Card titulo="#02 - Compontente com Parametro">
+      <CompParametro titulo="Esse é o Titulo" subtitulo="Esse é o Subtitulo" />
     </Card>
-    <Card titulo="Primeiro Componente">
-        <Primeiro />
+
+    <Card titulo="#01 - Primeiro Componente">
+      <Primeiro />
     </Card>
-        {/* <compFilhos>
+    {/* <compFilhos>
   
         </compFilhos> */}
-        {/* <Primeiro />
+    {/* <Primeiro />
         <CompParametro titulo="Esse é o titulo por parametro" 
             subtitulo="Esse é o subtitulo por paramentro"/>
 
         <CompParametro titulo="Opa" 
             subtitulo="Epa"/> */}
-
-    </div>
-
+  </div>
 );
